735357b2661f59ecaa22632d63cd0951278e3c02
Marxho <marxhop@hotmail.com>
2023-05-11 20:16:57 -0400

    add logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,28 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await initdb();
+  const transaction = db.transaction('jate', 'readwrite');
+  const store = transaction.objectStore('jate');
+  const request = store.add(content);
+  request.onsuccess = () => {
+    console.log('Content added to database');
+    };
+}
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await initdb();
+  const transaction = db.transaction('jate', 'readwrite');
+  const store = transaction.objectStore('jate');
+  const request = store.getAll();
+  request.onsuccess = () => {
+    
+    console.log('Content fetched from database', result.value);
+    return result.value;
+    }
+}
 
 initdb();
